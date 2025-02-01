b2f6f14174e952d648fa521466383546094bf56e
SeuNome <arafdiogo2009@hotmail.com>
2025-02-01 12:26:31 -0300

    Projetos Python 3

---
+++ Curso_em_video_Py3/aula8.py
@@ -0,0 +1,20 @@
+# import - doces
+#   IRA IMPORTAR A BIBLIOTECA TODA
+#       GENERALISTA
+#
+# from - import - doces - pudim
+#     IRA IMPORTAR SOMENTE O PUDIM
+#         VAI IMPORTAR SOMENTE A FUNCIONALIDADE QUE EU ESCOLHER - EXPECIFICA
+#
+# import mach - matematica
+#
+import math
+num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num,raiz))
+# from math import sqrt, floor
+# num = int(input('Digite um numero: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
+import emoji
+print(emoji.emojize('Ola, Mundo 🌎'))
